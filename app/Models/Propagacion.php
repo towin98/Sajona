ea71c918421fb8b73357d49a14129df6a9a5a9bf
towin <cristian3211998@gmail.com>
2021-12-13 22:43:04 -0500

    Se crea Requests (reglas de validacion) para Propagacion

--- app/Models/Propagacion.php
+++ app/Models/Propagacion.php
@@ -28,23 +28,4 @@ class Propagacion extends Model
         "pro_estado",
     ];
 
-    /**
-     * Reglas de validación para el recurso crear.
-     *
-     * @var array
-     */
-    public static $messagesValidators = [
-        'pro_fecha.required'                            => 'El campo fecha de propagación es requerido.',
-        'pro_fecha.date'                                => 'El tipo de formato del campo fecha de propagación debe ser ej: Y-m-d.',
-        'pro_tipo_propagacion.required'                 => 'El campo tipo de propagacion es requerido.',
-        'pro_tipo_propagacion.string'                   => 'El campo tipo de propagacion debe ser un string.',
-        'pro_variedad.required'                         => 'El campo variedad es requerido.',
-        'pro_variedad.numeric'                          => 'El campo variedad debe ser un número.',
-        'pro_tipo_incorporacion.required'               => 'El campo tipo incorporacion es requerido.',
-        'pro_tipo_incorporacion.string'                 => 'El campo tipo incorporacion debe ser un string.',
-        'pro_cantidad_material.required'                => 'El campo cantidad material es requerido.',
-        'pro_cantidad_material.numeric'                 => 'El campo cantidad material debe ser un número.',
-        'pro_cantidad_plantas_madres.required'          => 'El campo cantidad de plantas madres es requerido.',
-        'pro_cantidad_plantas_madres.numeric'           => 'El campo cantidad de plantas madres debe ser un número.',
-    ];
 }
